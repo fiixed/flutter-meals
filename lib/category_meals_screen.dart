e56d9c0a75edd139edbe21a556e709f6ff1e516d
Andrew Bell <fiixed@gmail.com>
2019-10-18 16:14:58 -0500

    meal model

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
   // final String categoryId;
   // final String categoryTitle;
   // const CategoryMealsScreen(this.categoryId, this.categoryTitle);
@@ -15,8 +16,10 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('The Recipes For The Category!'),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          
+        },
       ),
     );
   }
