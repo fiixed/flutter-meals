fff07c499b6856d098184463b6900442c226774d
Andrew Bell <fiixed@gmail.com>
2019-10-18 11:40:19 -0500

    named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals/category_meals_screen.dart';
 import './categories_screen.dart';
+import './category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,6 +30,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/categories-meals' : (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
